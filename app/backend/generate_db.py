564e65edd3bb0e501aac001bfbcb8569af1cb3a4
jeff776 <eitanlmh@gmail.com>
2025-08-06 06:29:11 +0300

    milestone-3 complete

--- app/backend/generate_db.py
+++ app/backend/generate_db.py
@@ -1,80 +1,40 @@
-import gzip
-
-from app.backend.modules.generate_db_modules.download_from_imdb import download_from_imdb
-# from app.backend.modules.generate_db_modules.data_parser import movie_ids, actor_ids, actor_per_movie
-from app.backend.modules.generate_db_modules.data_parser import actor_per_movie as apm
+import os
+from app.backend.modules.generate_db_modules.dataset_writer import dataset_writer
 from app.backend.modules.generate_db_modules.data_parser import movie_ids as mi
 from app.backend.modules.generate_db_modules.data_parser import actor_ids as ai
-from app.backend.modules.generate_db_modules.dataset_writer import dataset_writer
-import os
-import csv
-
-data_dir = None
-output_graph_pkl = None
-output_actor_name_to_id_pkl = None
-output_actor_name_to_movie_ids_pkl = None
+from app.backend.modules.generate_db_modules.data_parser import actor_per_movie as apm
 
 
-def generate_db(data_source):
-    global output_graph_pkl, output_actor_name_to_id_pkl, output_actor_name_to_movie_ids_pkl, data_dir
+def generate_db(movie_path_of_data_to_read, actor_path_of_data_to_read, movie_per_actor_path_to_read, path_to_write_data_to):
 
-    if data_source == "example":
-
-
-        data_dir = os.path.join(os.path.dirname(__file__), "data", "example_data")
-        example_actor_id_to_name_path = os.path.join(data_dir, "actor_id_to_name.csv")
-        example_movie_id_to_title_path = os.path.join(data_dir, "movie_id_to_title.csv")
-        example_actor_per_movie_path = os.path.join(data_dir, "actor_per_movie.tsv.gz")
-        example_parsed_data_dir = os.path.join(os.path.dirname(__file__), "data", "example_parsed_data")
-
-        output_graph_pkl = os.path.join(example_parsed_data_dir, "actor_graph_meta.pkl")
-        output_actor_name_to_id_pkl = os.path.join(example_parsed_data_dir, "actor_name_to_id.pkl")
-        output_actor_name_to_movie_ids_pkl = os.path.join(example_parsed_data_dir, "actor_name_to_movie_ids.pkl")
+    if os.path.exists(os.path.join(path_to_write_data_to, "dataset.pkl")):
+        print("all wanted data parsed")
+    else:
+        print("parsed data does not exist, starting to parse")
+        # parse all the data
+        movie_ids = mi(movie_path_of_data_to_read)
+        actor_ids = ai(actor_path_of_data_to_read)
+        actor_per_movie = apm(movie_ids, actor_ids, movie_per_actor_path_to_read)
+        # write the dataset we need in the right place to continue
+        dataset_writer(actor_per_movie, path_to_write_data_to)
 
 
-        example_actor_id_to_name = {}
-        example_movie_id_to_title = {}
 
-        with open(example_actor_id_to_name_path, "rt", encoding="utf-8") as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                example_actor_id_to_name[row["nconst"]] = row["primaryName"]
-
-        with open(example_movie_id_to_title_path, "rt", encoding="utf-8") as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                example_movie_id_to_title[row["tconst"]] = row["primaryTitle"]
-
-        dapm = apm(example_movie_id_to_title, example_actor_id_to_name, example_actor_per_movie_path)
-
-        dataset_writer(dapm, data_source)
-
-    if data_source == "real":
-        os.makedirs("data/parsed_data", exist_ok=True)
-
-        data_dir = os.path.join(os.path.dirname(__file__), "..", "data", "parsed_data")
-
-        output_graph_pkl = os.path.join(data_dir, "actor_graph_meta.pkl")
-        output_actor_name_to_id_pkl = os.path.join(data_dir, "actor_name_to_id.pkl")
-        output_actor_name_to_movie_ids_pkl = os.path.join(data_dir, "actor_name_to_movie_ids.pkl")
+movie_example_path_to_read = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data", "example_data", "movie_id_to_title.tsv.gz")
+actor_example_path_to_read = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data", "example_data", "actor_id_to_name.tsv.gz")
+movie_per_actor_example_path_to_read = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data", "example_data", "actor_per_movie.tsv.gz")
+example_path_to_write = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data", "example_parsed_data")
 
 
-        if os.path.exists(output_graph_pkl) and os.path.exists(output_actor_name_to_id_pkl) and os.path.exists(
-                output_actor_name_to_movie_ids_pkl):
-            print("pkls already exists. skipping building pickles...")
+generate_db(movie_example_path_to_read, actor_example_path_to_read, movie_per_actor_example_path_to_read, example_path_to_write)
 
-        else:
-            print("pkl files not found. generating pickles...")
-            data_dir = "data/downloaded_data"  # This is where our data is.
 
-            download_from_imdb()  # download data from imdb files
-            actor_per_movie_path = os.path.join(data_dir, "title_principals.tsv.gz")
-            # parse all the data
-            movie_ids = mi()
-            actor_ids = ai()
-            actor_per_movie = apm(movie_ids, actor_ids, actor_per_movie_path)
 
-            # write the dataset we need to continue, if needed it can recieve 'max_movie'
-            dataset_writer(actor_per_movie, data_source)
 
-            print("pickles generated and saved")
+movie_real_path_to_read = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data", "real_data", "title_basics.tsv.gz")
+actor_real_path_to_read = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data", "real_data", "name_basics.tsv.gz")
+movie_per_actor_real_path_to_read = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data", "real_data", "title_principals.tsv.gz")
+real_path_to_write = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data", "real_parsed_data")
+
+
+generate_db(movie_real_path_to_read, actor_real_path_to_read, movie_per_actor_real_path_to_read, real_path_to_write)
--- app/frontend/app.py
+++ app/frontend/app.py
@@ -1,62 +1,46 @@
-import time
 from flask import Flask, render_template, request
 import threading
 import os
-from app.backend.bacon_distance import get_bacon_distance
-import pickle
-
+from app.backend.bacon_distance import load_graph_from_pkl, find_actor_id_by_name, bfs_actor_distance
 from app.backend.generate_db import generate_db
 
 app = Flask(__name__)
 
 #global vars to hold the loaded data
-example_graph = None
-example_actor_name_to_id = None
 
 graph = None
-actor_name_to_id = None
-actor_name_to_movie_ids = None
-
 data_source = None
-
-example_graph_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "backend", "data", "example_parsed_data", "actor_graph_meta.pkl"))
-real_graph_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "backend", "data", "parsed_data", "actor_graph_meta.pkl"))
-
-
+bacon_id = None
 @app.route("/", methods=["GET"])
 def choose_data():
     return render_template("choose_data.html")
 
 @app.route("/distance", methods=["GET", "POST"])
 def main():
-    global graph, actor_name_to_id, actor_name_to_movie_ids, data_source, example_graph_path, real_graph_path
+    global graph, data_source, bacon_id
 
     if request.method == "GET":
         data_source = request.args.get("data_source")
         print(f"data_source: {data_source}")
 
         def load_data():
-            global graph, actor_name_to_id
-
+            global graph, bacon_id
             if data_source == "example":
                 base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "backend", "data", "example_parsed_data"))
+                bacon_id = "nm0000002"
             else:
-                base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "backend", "data", "parsed_data"))
+                base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "backend", "data", "real_parsed_data"))
+                bacon_id = "nm0000102"
 
-            graph_path = os.path.join(base_path, "actor_graph_meta.pkl")
-            actor_name_to_id_path = os.path.join(base_path, "actor_name_to_id.pkl")
+            graph_path = os.path.join(base_path, "dataset.pkl")
 
-            if not (os.path.exists(graph_path) and os.path.exists(actor_name_to_id_path)):
+            if not (os.path.exists(graph_path)):
                 print(f"{data_source} dataset not found. Running generate_db({data_source})...")
                 generate_db(data_source)
             else:
                 print(f"{data_source} cached dataset found. Skipping generate_db({data_source})...")
 
-            with open(graph_path, "rb") as f:
-                graph = pickle.load(f)
-
-            with open(actor_name_to_id_path, "rb") as f:
-                actor_name_to_id = pickle.load(f)
+            graph = load_graph_from_pkl(graph_path)
 
             print("Cache loaded")
 
@@ -71,19 +55,24 @@ def main():
     if request.method == "POST":
         actor_name = request.form.get("actor_name")
 
-        if actor_name and graph and actor_name_to_id:
-            distance = get_bacon_distance(actor_name, graph, actor_name_to_id)
+        if actor_name and graph:
+            target_actor_id = find_actor_id_by_name(graph, actor_name)
 
-
-            if isinstance(distance, int):
-                if distance == 0:
-                    finale_answer = "This is Kevin Bacon!"
-                elif distance == float("inf"):
-                    finale_answer = f"No connection was found between Kevin Bacon and {actor_name}"
-                else:
-                    finale_answer = f"Bacon distance for {actor_name} is {distance}"
+            if not target_actor_id:
+                finale_answer = f"{actor_name} actor is not found! try again"
             else:
-                finale_answer = "No actor name was provided"
+                distance = bfs_actor_distance(graph, bacon_id, target_actor_id)
+
+
+                if isinstance(distance, int):
+                    if distance == 0:
+                        finale_answer = "This is Kevin Bacon!"
+                    elif distance == float("inf"):
+                        finale_answer = f"No connection was found between Kevin Bacon and {actor_name}"
+                    else:
+                        finale_answer = f"Bacon distance for {actor_name} is {distance}"
+                else:
+                    finale_answer = "No actor name was provided"
 
     return render_template("main_page.html", result=finale_answer)
 
